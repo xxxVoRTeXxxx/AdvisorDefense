9e5282d5403f2516ab948d536a4c4050ab75b3b1
AdvisorDefense <philip.coniglio@gmail.com>
2025-07-13 18:12:22 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('mobile-menu').classList.toggle('hidden');
+  });
+});
